102bdb4e434a7df2b8f8cc45c0266c2c51bfffd5
ChristopherS24 <chryskies@gmail.com>
2024-10-21 19:18:18 +0200

    correzioni

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('title', 'Dc Comics | Home')
+
+@section('main')
+    <main>
+        <div>
+            <h2>
+                Current Series
+            </h2>
+            
+            <div class="row">
+
+                @foreach ($comics as $element)
+                    <div class="col-2 p-10">
+                        <figure>
+                            <img src=" {{ $element['thumb'] }} " alt=" {{ $element['title'] }} " width=250"">
+                        </figure>
+
+                        <h4>
+                            {{ $element['title'] }}
+                        </h4>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+
+        <div>
+            <div class="row">
+                <ul>
+                    @foreach ($itemsMenu as $item)
+                        <li>
+                            <figure>
+                                <img src=" {{ Vite::asset('resources/images/' . $item['image']) }} " alt="" width="75">
+                            </figure>
+                            <span>
+                                {{ $item['name'] }}
+                            </span>
+                        </li>
+                    @endforeach
+                </ul>
+            </div>
+        </div>
+    </main>
+@endsection
